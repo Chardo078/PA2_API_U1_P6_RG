8a6d8e516ee19400e7be9016909f25997b8f0292
Chardo078 <richardgonzaga078@gmail.com>
2026-04-15 19:50:56 -0500

    Add files via upload

---
+++ PA2_API_U1_P6_DP-main/PA2_API_U1_P6_DP-main/src/main/java/uce/edu/pa2/api/bodega/Main.java
@@ -0,0 +1,62 @@
+package uce.edu.pa2.api.bodega;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import jakarta.enterprise.inject.spi.CDI;
+import jakarta.inject.Inject;
+
+@QuarkusMain
+public class Main {
+
+
+ 
+    public static void main(String... args) {
+ 
+        Quarkus.run(App.class, args);
+ 
+    }
+ 
+    public static class App implements QuarkusApplication {
+ 
+        // modelos de IoC
+        // 1 DI
+        @Inject
+        private PedidoService pedidoService;
+ 
+        // 2. Service Locator LookuP
+        // con el current accedo al contenedor etsoy buscando manualmente la dependencia
+        // private PedidoService pedidoService =
+        // CDI.current().select(PedidoService.class).get();
+ 
+        @Override
+        public int run(String... args) {
+            PedidoService pedidoService = CDI.current().select(PedidoService.class).get();
+ 
+            System.out.println("caso 1 ----------------------");
+            Pedido pedido = new Pedido("Deyvi Pilataxi", "Libro", 3, "ragonzaga@uce.edu.ec");
+ 
+            // PedidoService service = new PedidoService();
+            pedidoService.registrar(pedido);
+ 
+            // caso 2
+ 
+            /*
+             * System.out.println("caso 2 -----------------------");
+             * Pedido pedido2 = new Pedido("Joel", "jugo", 120, "djpilataxi@uce.edu.ec");
+             * pedidoService.registrar(pedido2);
+             *
+             * System.out.println("caso 3-------------------------");
+             * Pedido pedido3 = new Pedido("maria", "agua", 75, "deyvi@uce.edu.ec");
+             * pedidoService.registrar(pedido3);
+             */
+ 
+            return 0;
+        }
+ 
+    }
+ 
+}
